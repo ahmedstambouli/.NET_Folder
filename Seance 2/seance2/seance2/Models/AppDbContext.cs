78c05e03885ae4badd491adc6242ea1fdc3d910e
ahmed STANBOULI <ahmed.stambouli98@gmail.com>
2024-02-19 09:44:32 +0100

    update

---
+++ Seance 2/seance2/seance2/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace seance2.Models
+{
+	public class AppDbContext:DbContext
+	{
+		public AppDbContext(DbContextOptions options):
+						base(options)
+		{
+
+		}
+
+
+		public DbSet<Movie> movies { get; set; }
+	}
+}
--- Seance 2/seance2/seance2/Program.cs
+++ Seance 2/seance2/seance2/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using seance2.Models;
+
 namespace seance2
 {
     public class Program
@@ -10,10 +13,37 @@ namespace seance2
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            //pour connection a la base de donnee
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+			//ken jatte mel loulle raw eli ba3dha maye5dmouch
+			//app.UseWelcomePage("/"); 
+
+
+			//ken famech path tjiblek hello ahmed 
+			//app.MapGet("/", () =>"hello ahmed" );
+
+            //return sur page ficher json qui  appartien id,name de movie 
+			//app.MapGet("Movietest", () => new Movie() { name="Movie1"});
+
+
+            //app.MapGet("Product/test1", () => "test1");
+
+
+			//product?name=ahmed return ahmed
+			//app.MapGet("product", (string name) => name);
+
+
+            //app.MapGet("/test",()=>"hello world").WithName("Hello");
+
+            //redirect vers le app de name hello
+            //app.MapGet("/redirect-me", () => Results.RedirectToRoute("hello"));
+        
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
